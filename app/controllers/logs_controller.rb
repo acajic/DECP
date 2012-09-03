873638458f41dc6f1fde6508178b37414f0b4701
Andrija Cajic <ancajic@gmail.com>
2012-09-03 19:05:35 +0200

    logs uljepsan

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,7 +2,7 @@ class LogsController < ApplicationController
   # GET /logs
   # GET /logs.json
   def index
-    @logs = Log.all
+    @logs = Log.order("created_at DESC")
 
     respond_to do |format|
       format.html # index.html.erb
